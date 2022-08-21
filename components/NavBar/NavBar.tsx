690630f8f707d267807bf7cc0a19dab7bf89e414
Stephen Kempisty <stephen.kempisty@caa.com>
2022-08-21 14:40:23 -0700

    Conditionally show navbar elements depending on view

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,25 +1,44 @@
 import { FC } from "react";
+import styled from "styled-components";
+import Box from "../shared/styled-system/Box";
 import Flex from "../shared/styled-system/Flex";
-import {Button, Icon} from "semantic-ui-react";
 import Link from "next/link";
+import {Button, Icon} from "semantic-ui-react";
+import { useRouter } from "next/router";
 
+
+interface IVisibilityBoxProps {
+  visible: boolean;
+}
+
+const VisibilityBox = styled(Box)<IVisibilityBoxProps>`
+  visibility: ${props => props.visible ? "visible" : "hidden"};
+`
 
 const NavBar: FC = () => {
+  const { pathname } = useRouter()
+  const isOnHome = pathname === "/"
+  const isOnMyList = pathname === "/my-list"
+
   return (
     <Flex
       justifyContent="space-between"
       padding="2em"
     >
-      <Link href={"/"}>
-        <Button>
+      <VisibilityBox visible={!isOnHome}>
+        <Link href={"/"}>
+          <Button>
           <Icon name="chevron left" />
           Back to Search
-        </Button>
-      </Link>
+          </Button>
+        </Link>
+      </VisibilityBox>
 
-      <Link href={"/my-list"}>
-        View My List
-      </Link>
+      <VisibilityBox visible={!isOnMyList}>
+        <Link href={"/my-list"}>
+          View My List
+        </Link>
+      </VisibilityBox>
     </Flex>
   )
 }
