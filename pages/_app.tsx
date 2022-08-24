30af78fd784a9f46207e7ff9bd8fb002591a5d42
Stephen Kempisty <stephenkempisty@Stephens-MacBook-Pro.local>
2022-08-23 19:51:06 -0700

    Adds nextjs progress bar for a little better ux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 import type { AppProps } from 'next/app'
+import NextNProgress from "nextjs-progressbar";
 import Box from "../components/shared/styled-system/Box";
 import NavBar from "../components/NavBar";
+import { Provider } from "react-redux";
 import { store } from '../store'
-import {Provider} from "react-redux";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <NextNProgress options={{ showSpinner: false }}/>
       <NavBar/>
 
       <Box padding="4em 8em">
